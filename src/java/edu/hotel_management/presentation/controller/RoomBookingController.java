6e058179edc088ed175d8a59cba6c0cd425b6b9d
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-10-13 13:21:40 +0700

    fix: migrate controller to the right place

--- src/java/edu/hotel_management/presentation/controller/RoomBookingController.java
+++ src/java/edu/hotel_management/presentation/controller/RoomBookingController.java
@@ -2,13 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package edu.hotel_management.infrastructure.controller;
+package edu.hotel_management.presentation.controller;
 
 import edu.hotel_management.presentation.constants.IConstant;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,32 +18,53 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author TR_NGHIA
  */
-
-public class HomeController extends HttpServlet {
+@WebServlet(name = "RoomBookingController", urlPatterns = {"/RoomBookingController"})
+public class RoomBookingController extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
-            
-            RequestDispatcher rd = request.getRequestDispatcher(IConstant.PAGE_HOME);
+           
+            RequestDispatcher rd = request.getRequestDispatcher(IConstant.PAGE_ROOM_BOOKING);
             rd.forward(request, response);
         }
     }
 
-   
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
